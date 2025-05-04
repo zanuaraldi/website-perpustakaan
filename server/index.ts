b5953880b42bd3a482392108330c9be32760dbcd
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-04 21:33:00 +0700

    setting drizzle

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,17 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import { db } from "../db"; // asumsi path db-mu
+import { db } from "../db";
 
 const app = new Hono();
 
 app.get("/api/hello", (c) => {
   return c.json({ message: "Hello from Hono API!" });
 });
+
+app.get("/test", async (c) =>{
+  const result = await db.execute("select 1")
+  return c.json(result, 200)
+})
 
 // Jalankan server di port 3000
 serve({
