848cc483b9e48b85328a42cdab36c6fe872b2d19
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-04 23:40:19 +0700

    schema

---
+++ db/schema.ts
@@ -0,0 +1,88 @@
+
+import { relations } from "drizzle-orm";
+import { pgTable, serial, varchar,timestamp ,numeric, integer, text, char, boolean } from "drizzle-orm/pg-core";
+
+export const kategori = pgTable("kategori", {
+    idKategori: serial("id_kategori").primaryKey(),
+    kodeKategori: varchar("kode_kategori").notNull(),
+    namaKategori: varchar("nama_kategori").notNull(),
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const penerbit = pgTable("penerbit", {
+    idPenerbit: serial("id_penerbit").primaryKey(),
+    namaPenerbit: varchar("nama_penerbit").notNull(),
+    reatedAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const pembuat = pgTable("pembuat", {
+    idPembuat: serial("id_pembuat").primaryKey(),
+    namaPembuat: varchar("nama_pembuat").notNull(),
+    reatedAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const buku = pgTable("buku", {
+    idBuku: serial("id_buku").primaryKey(),
+    idKategori: integer("id_kategori")
+        .notNull()
+        .references(() => kategori.idKategori, {
+            onDelete: "cascade",
+            onUpdate: "cascade"
+        }),
+    idPenerbit: integer("id_penerbit")
+        .notNull()
+        .references(() => penerbit.idPenerbit, {
+            onDelete: "cascade",
+            onUpdate: "cascade"
+        }),
+    idPembuat: integer("id_pembuat")
+        .notNull()
+        .references(() => pembuat.idPembuat, {
+            onDelete: "cascade",
+            onUpdate: "cascade"
+        }),
+    isbn: varchar("isbn").notNull(),
+    issn: varchar("issn").notNull(),
+    tahunPembuatan: char("tahun_pembuatan", {length: 4}).notNull(),
+    harga: numeric("harga").notNull(),
+    keterangan: text("keterangan"),
+    tersedia: boolean("tersedia").notNull(),
+    reatedAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const relasiKategoriToBuku = relations(kategori, ({ many }) => ({
+    kategori: many(buku)
+}))
+
+export const relasiBukuToKategori = relations(buku, ({ one }) =>  ({
+    kategori: one(kategori, {
+        fields: [buku.idKategori],
+        references: [kategori.idKategori]
+    })
+}))
+
+export const relasiPenerbitToBuku = relations(penerbit, ({ many })=>({
+    penerbit: many(buku)
+}))
+
+export const relasiBukuToPenerbit = relations(buku, ({ one }) =>({
+    penerbit: one(penerbit, {
+        fields: [buku.idPenerbit],
+        references: [penerbit.idPenerbit]
+    })
+}))
+
+export const relasiPembuatToBuku = relations(pembuat, ({ many })=>({
+    pembuat: many(buku)
+}))
+
+export const relasiBukuToPembuat = relations(buku, ({ one }) =>({
+    pembuat: one(pembuat, {
+        fields: [buku.idPembuat],
+        references: [pembuat.idPembuat]
+    })
+}))
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/db/schema.ts',
+  schema: './db/schema.ts',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   }
